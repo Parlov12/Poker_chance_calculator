244a7fc5d45a7f7f36490180dda2e15325586f28
Petar Parlov <petarparlov4@gmail.com>
2021-05-28 00:14:46 +0200

    13th commit

--- app/src/main/java/com/example/pokerchancecalculator/Test.java
+++ app/src/main/java/com/example/pokerchancecalculator/Test.java
@@ -19,13 +19,37 @@ public class Test extends AppCompatActivity {
 
         Button calc = findViewById(R.id.calculate);
         TextView ispis = findViewById(R.id.ispis);
-        EditText karte = findViewById(R.id.karte);
+        EditText karta1 = findViewById(R.id.karte);
+        EditText karta2 = findViewById(R.id.karte);
+        EditText karta3 = findViewById(R.id.karte);
+        EditText board = findViewById(R.id.karte);
+
         TestConsole c = null;
         String[] unos = {"-b","7d9dTs","JhJs","AdJd","KcQd"};
 
         calc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if(karta1.getText().toString() != "")
+                {
+                    unos[2] = karta1.getText().toString();
+                }
+                if(karta2.getText().toString() != "")
+                {
+                    unos[3] = karta2.getText().toString();
+                }
+                if(karta3.getText().toString() != "")
+                {
+                    unos[4] = karta3.getText().toString();
+                }
+
+                if(board.getText().toString() != "")
+                {
+                    unos[1] = board.getText().toString();
+                }
+
+
 
                 try {
                     c.test(unos, ispis);
